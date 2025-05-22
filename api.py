ca6456e838f8ac29926ff4c4bf45c4633468e389
B E N T E X <118435954+mrbentex@users.noreply.github.com>
2025-05-22 15:39:39 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -108,4 +109,5 @@ def check_card():
     })
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
